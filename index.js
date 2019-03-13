3a50084ac39274f200c4597a52ed0ebeed757a93
WebWalterWrite <webwalterwhrite@gmail.com>
2019-03-13 13:33:07 +0100

    type: applicatif desc: config packages

---
+++ index.js
@@ -0,0 +1,33 @@
+import "@babel/polyfill";
+import express from 'express'
+import bodyParser from 'body-parser'
+import imageRoute from './src/Routes/uploadRoute'
+import path from 'path';
+import exphbs from 'express-handlebars'
+
+
+const app = express()
+
+// static files rendering
+const chemin = path.join(__dirname, '/src/public');
+app.use('/public', express.static(chemin));
+
+// middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// templates config
+app.engine('.hbs', exphbs({
+    defaultLayout:'layout',
+    extname:'.hbs'})
+)
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname,'src/views'));
+
+// express routes
+app.use('/', imageRoute)
+
+// run server
+app.listen(3000, () => console.log("it's great"));
+
+export default app;
