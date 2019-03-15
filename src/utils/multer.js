c3a0a87e5f1e490c96089e68d2817f36350a69f2
WebWalterWrite <webwalterwhrite@gmail.com>
2019-03-15 17:55:42 +0100

    type:function desc: add fn isFileType / check type mime photo et ext du fichier

--- src/utils/multer.js
+++ src/utils/multer.js
@@ -1,26 +1,45 @@
 import multer from 'multer'
+import path from 'path';
+
 
 const storage = multer.diskStorage({ 
-    destination:(req, file, cb) => cb (null, './src/storage'),
-    filename: (req, file, cb) => cb (null, `${req.body.filename}-${Date.now()}.jpg`)
+    destination:(req, file, cb) => cb (null, './src/public/upload'),
+    filename: (req, file, cb) => cb (null, `${file.fieldname}-${Date.now()}.jpg`)
 })
+
+
+/**
+Vérifier le type de fichier.
+ */
+
+const isFileType = (file, cb) => {
+
+// type requis
+    const fileExt = ['.jpeg','.jpg', '.png', '.gif']
+    const mimeType = ['image/jpeg', 'image/png']
+
+// type du fichier reçu
+    const mime = file.mimetype;
+    const ext= path.extname(file.originalname)
+
+// test type
+    const ifType = mimeType.includes(mime)
+    const ifExt = fileExt.includes(ext)
+
+    if(ifType && ifExt){
+        return cb(null, true)
+    }
+    else{
+        let err = { message: 'Le fichier transmis doit être de type jpg, png, jpeg'}
+        return cb(err, false)
+    }
+}
+
 
 export const upload = multer({
     storage: storage,
     limits: {
-        fileSize: 400000
+        fileSize: 8 * 1024 * 1024
     },
     fileFilter: (req, file, cb) => isFileType(file, cb)
 });
-
-/**
- * 
- * @param {*} file 
- * @param {*} cb 
- */
-
-const isFileType = (file, cb) => {
-    const filetypes = ['jpeg','jpg', 'png', 'gif'];
-    const mimeType = file.mimetype;
-    const extName = path.extname()
-}
