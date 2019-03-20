a7bb6dd77d9a1fcbd51212dd0bfcfd98bbc36b21
WebWalterWrite <webwalterwhrite@gmail.com>
2019-03-20 12:54:04 +0100

    type: route desc: ajout route '/profile' pour récupérer la photo de profil

--- src/Routes/uploadRoute.js
+++ src/Routes/uploadRoute.js
@@ -1,9 +1,23 @@
 import router from './config';
-import { home, storeImage } from '../Controllers/uploadControllers';
+import { home, getProfile,  storeImage } from '../Controllers/uploadControllers';
 
+/**
+ * @description Renvoi page d'accueil
+ */
 router.get('/',home )
 
+
+/**
+ * @description -récupérere la photo de profil
+ */
+router.get('/profile', getProfile)
+
+/**
+ * @description - insert nouvelle image de profile
+ */
 router.post('/upload',storeImage)
+
+
 
 
 export default router
