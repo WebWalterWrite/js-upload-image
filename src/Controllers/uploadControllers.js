3939a5b45470d2a81905aa74c88ccd4f44982a27
WebWalterWrite <webwalterwhrite@gmail.com>
2019-03-20 17:55:35 +0100

    type: controller desc: ajout deleteImage fn / suppression photo de profile // refactor getProfile fn

--- src/Controllers/uploadControllers.js
+++ src/Controllers/uploadControllers.js
@@ -1,11 +1,15 @@
 import upload from "../utils/multer";
 import { getImg, removeImg } from "../utils/fileSystem";
 
-export const storeImage = async (req, res) => {
-	const img = await getImg();
 
-	if (img && img.length > 0) {
-		await removeImg(...img)
+/**
+ * @description Traite l'image reçue. Stock l'image ou supprime l'ancienne image si existante et la remplace par la nouvelle
+ * @param {object} res Renvoi un json contenant le message err or success / path de l'image
+ */
+export const storeImage = async (req, res) => {
+	const img = await getImg(); // Vérifier si une image est déjà présente
+
+	img.length > 0 && await removeImg(...img); // Si oui supprimer l'image
 
 	const multer = upload.single("profile");
 
@@ -20,10 +24,13 @@ export const storeImage = async (req, res) => {
 					img: `upload/${req.file.filename}`
 			  });
     });
-    }
+    
 };
 
 
+/**
+ * @description - Renvoi la page accueil
+ */
 export const home = (req, res) => {
 
 	res.render("homepage");
@@ -31,10 +38,34 @@ export const home = (req, res) => {
 
 
 
+/**
+ * @description  récupére la photo du dossier /upload.
+ * @param {*} req 
+ * @param {object} res renvoi une réponse au format json
+ */
 export const getProfile = async (req, res) => {
 
     const img = await getImg()
+		const image = img.length > 0 ? img : false 
+    res.json({img:image})
 
-    res.json({img:img})
+}
+
+/**
+ * @description Supprime la photo du dossier /upload
+ * @param {object} req récupérer le l'objet img
+ * @param {*} res 
+ */
+export const deleteImage = async (req, res) => {
+	try {
+		const val =req.body.img
+		await removeImg(val); // Supprime l'image
+
+		res.json({
+      msg: "La photo a bien été supprimée",
+    });
+	} catch (error) {
+			console.log('Une erreur est survenue deleteImage fn :', error.message)
+	}
 
 }
