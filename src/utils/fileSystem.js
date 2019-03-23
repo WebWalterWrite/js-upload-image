bc68131b0e79be11d9ae3f5215954f4da9f77365
WebWalterWrite <webwalterwhrite@gmail.com>
2019-03-23 13:23:27 +0100

    type: fn desc: add fn getUser

--- src/utils/fileSystem.js
+++ src/utils/fileSystem.js
@@ -1,8 +1,20 @@
 import fs, { promises } from 'fs'
 import path from 'path'
-const { readdir, unlink } = promises
+const { readdir, readFile, unlink } = promises
 
 const pathImg = path.join(__dirname, '../public/upload/')
+
+
+
+export const getUser = async () => {
+    const pathUser = path.join(__dirname, '../storage/user.json');
+    try {
+        const user = await readFile(pathUser,'utf8');
+        return JSON.parse(user)
+    } catch (error) {
+        console.log("L'erreur getUser fn suivante est survenue: ", error.message); 
+    }
+};
 
 
 export const getImg = async () => {
@@ -17,7 +29,6 @@ export const getImg = async () => {
 
 export const removeImg = async (val) => {
     try {
-        console.log(val)
         const ifDelete = val.includes('../public/upload/');
 
         const fullPath = ifDelete && path.join(__dirname,val);
